3410aecc475adc590cd230b5ed05ade89d26ba59
fernando-iwerson <fernandoiwerson@gmail.com>
2023-08-17 17:22:15 -0300

    first commit

---
+++ tiposVariaveis.js
@@ -0,0 +1,21 @@
+let maiorDeIdade,nome,bigInt,meuSimbolo;
+console.log(`${maiorDeIdade} ${nome} ${bigInt} ${meuSimbolo}`);
+
+maiorDeIdade = null;
+nome = null;
+bigInt = null;
+meuSimbolo = null;
+
+console.log(`${maiorDeIdade} ${nome} ${bigInt} ${meuSimbolo}`);
+
+maiorDeIdade = true;
+nome = "Fernando";
+bigInt = 192922838838484n;
+meuSimbolo = Symbol("meu simbolo");
+
+console.log(maiorDeIdade, "do tipo:",typeof maiorDeIdade);
+console.log(nome, "do tipo:",typeof nome);
+console.log(bigInt, "do tipo:",typeof bigInt);
+console.log(meuSimbolo, "do tipo:",typeof meuSimbolo);
+
+
